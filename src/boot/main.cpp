5e5df530e8e20328300775219a7deec689b73d61
Lance Hart <shipsimfan@hotmail.com>
2021-02-25 17:15:40 -0500

    Moved c++ language features to different file

--- src/boot/main.cpp
+++ src/boot/main.cpp
@@ -11,60 +11,6 @@ extern "C" void kmain() {
     Console::Println("Total Memory: %i MB", (Memory::Physical::GetTotalPages() * PAGE_SIZE) / MEGABYTE);
     Console::Println("Free Memory: %i MB", (Memory::Physical::GetFreePages() * PAGE_SIZE) / MEGABYTE);
 
-    uint64_t pid = Fork();
-
-    if (pid == 0) {
-        Console::Println("Inside child process (%i)", currentProcess->id);
-        Exit(0x0BADC0DE);
-    } else {
-        Console::Println("The childs id is %i", pid);
-        uint64_t status = Wait(pid);
-        Console::Println("Exit status %#llX", status);
-    }
-
-    while (1)
+        while (1)
         asm volatile("hlt");
-}
-
-extern "C" void __cxa_pure_virtual() { panic("Attempting to invocate a pure virtual function!"); }
-
-struct atexitFuncEntry {
-    void (*destructorFunc)(void*);
-    void* objPtr;
-    void* dsoHandle;
-};
-
-atexitFuncEntry __atexitFuncs[128];
-uint64_t __atexitFuncCount = 0;
-
-void* __dso_handle;
-
-extern "C" int __cxa_atexit(void (*f)(void*), void* objPtr, void* dso) {
-    if (__atexitFuncCount >= 128)
-        return -1;
-
-    __atexitFuncs[__atexitFuncCount].destructorFunc = f;
-    __atexitFuncs[__atexitFuncCount].objPtr = objPtr;
-    __atexitFuncs[__atexitFuncCount].dsoHandle = dso;
-
-    return 0;
-}
-
-extern "C" void __cxa_finalize(void* f) {
-    uint64_t i = __atexitFuncCount;
-    if (!f) {
-        while (i--) {
-            if (__atexitFuncs[i].destructorFunc)
-                __atexitFuncs[i].destructorFunc(__atexitFuncs[i].objPtr);
-        }
-
-        return;
-    }
-
-    while (i--) {
-        if (__atexitFuncs[i].destructorFunc == f) {
-            __atexitFuncs[i].destructorFunc(__atexitFuncs[i].objPtr);
-            __atexitFuncs[i].destructorFunc = 0;
-        }
-    }
 }
---
+++ src/cpp.cpp
@@ -0,0 +1,45 @@
+#include <panic.h>
+#include <stdint.h>
+
+extern "C" void __cxa_pure_virtual() { panic("Attempting to invocate a pure virtual function!"); }
+
+struct atexitFuncEntry {
+    void (*destructorFunc)(void*);
+    void* objPtr;
+    void* dsoHandle;
+};
+
+atexitFuncEntry __atexitFuncs[128];
+uint64_t __atexitFuncCount = 0;
+
+void* __dso_handle;
+
+extern "C" int __cxa_atexit(void (*f)(void*), void* objPtr, void* dso) {
+    if (__atexitFuncCount >= 128)
+        return -1;
+
+    __atexitFuncs[__atexitFuncCount].destructorFunc = f;
+    __atexitFuncs[__atexitFuncCount].objPtr = objPtr;
+    __atexitFuncs[__atexitFuncCount].dsoHandle = dso;
+
+    return 0;
+}
+
+extern "C" void __cxa_finalize(void* f) {
+    uint64_t i = __atexitFuncCount;
+    if (!f) {
+        while (i--) {
+            if (__atexitFuncs[i].destructorFunc)
+                __atexitFuncs[i].destructorFunc(__atexitFuncs[i].objPtr);
+        }
+
+        return;
+    }
+
+    while (i--) {
+        if (__atexitFuncs[i].destructorFunc == f) {
+            __atexitFuncs[i].destructorFunc(__atexitFuncs[i].objPtr);
+            __atexitFuncs[i].destructorFunc = 0;
+        }
+    }
+}
